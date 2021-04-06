c19bbb7b9f4548b1f10312b723720c94f702e2ed
Alexie81 <alexie.popescu2019@yahoo.com>
2021-04-06 17:51:15 +0300

    Finish errors

--- public/admin_besttkd_mihaisorin/js/galery.js
+++ public/admin_besttkd_mihaisorin/js/galery.js
@@ -1,5 +1,5 @@
 var storage = firebase.storage();
-var storageRef = storage.ref();
+var storageRef = firebase.storage().ref();
 var i = 0;
 //Start code
 storageRef.child('imagini').listAll().then(function(result){
@@ -64,3 +64,49 @@ function readURL(input) {
 //     e.stopPropagation();
 //     console.log("sdads")
 // });
+
+function uploadImageFirebase(){
+    if(document.getElementById('titlu').value !== "" && document.getElementById('descriere').value !== ""){
+    const ref = firebase.storage().ref();
+    const file = document.querySelector('#dsfsfdsdfs').files[0];
+    const nameFile = file.name;
+    var tit1 = null;
+    var descTd = null;
+    console.log(nameFile);
+    const metadata = {
+      contentType:file.type
+    }
+    st1 = storageRef.child('imagini/'+nameFile)
+    console.log(st1)
+    const task = storageRef.child("images/"+nameFile).put(file, metadata);
+    task
+    .then(snapshot => {
+        snapshot.ref.getDownloadURL();
+        tit1 = document.getElementById('titlu').value;
+        descTd = document.getElementById('descriere').value;
+    })
+    .then(url => {
+    firebase.database().ref('imagini').child(nameFile).set({
+        url: url,
+        titlu: tit1,
+        desc: descTd
+    });
+    });
+} else {
+    if(document.getElementById('titlu').value === ""){
+    Swal.fire({
+        icon: 'error',
+        title: 'Completeaza titlul !',
+        text: '',
+        footer: ''
+      })
+    } else if(document.getElementById('descriere').value === ""){
+        Swal.fire({
+            icon: 'error',
+            title: 'Completeaza descrierea!',
+            text: '',
+            footer: ''
+          })
+    }
+}
+}
